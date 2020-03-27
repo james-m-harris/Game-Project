083329af741f5e6efd2cc9454196199c287a5619
james-m-harris <61802957+james-m-harris@users.noreply.github.com>
2020-03-27 13:59:41 -0700

    Speeding Bus Animation
    I am trying to make a speeding bug animtion and I am struggling with getting my animation to even show up. My drawiangs are done and in my code so I dont know what I am doing wrong.

--- 5-animated-sprites/sketch.js
+++ 5-animated-sprites/sketch.js
@@ -2,7 +2,7 @@
 //
 // animations like p5 images should be stored in variables
 // in order to be displayed during the draw cycle
-var ghost;
+var fastbus;
 //
 // //it's advisable (but not necessary) to load the images in the preload function
 // //of your sketch otherwise they may appear with a little delay
@@ -10,7 +10,7 @@ function preload() {
 //
 //   //create an animation from a sequence of numbered images
 //   //pass the first and the last file name and it will try to find the ones in between
-  ghost = loadAnimation('assets/ghost_standing0001.png', 'assets/ghost_standing0007.png');
+  fastbus = loadAnimation('sprites/fastbus002.png', 'sprites/fastbus003.png', 'sprites/fastbus0034.png','sprites/fastbus005.png', 'sprites/fastbus006.png', 'sprites/fastbus007.png', 'sprites/fastbus008.png', 'sprites/fastbus001.png'
 
 }
 
@@ -23,6 +23,6 @@ function draw() {
 
   //specify the animation instance and its x,y position
   //animation() will update the animation frame as well
-  animation(ghost, 300, 150);
+  animation(fastbus, 300, 150);
 
 }
